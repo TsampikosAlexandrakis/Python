a54979a35114494e5d5a1f09c44e5f9eb57dd872
TsampikosAlexandrakis <53975355+TsampikosAlexandrakis@users.noreply.github.com>
2019-10-02 16:19:40 +0300

    Update YoutubeDownloader.py

--- YoutubeDownloader.py
+++ YoutubeDownloader.py
@@ -1,16 +1,18 @@
 from subprocess import call
 from time import sleep
 
-url = input("Video URL : ")
-file_type = input("Type mp3 for audio and mp4 for video : ")
-
-if file_type == "mp3" or file_type == "MP3":
+def download():
+  if file_type == "mp3" or file_type == "MP3":
     command = str("youtube-dl -x --audio-format mp3 --audio-quality 0 " + url + " -c")
     call(command)
-elif file_type == "mp4" or file_type == "MP4":
+  elif file_type == "mp4" or file_type == "MP4":
     command = str("youtube-dl --recode-video mp4 " + url + " -c")
     call(command)
-else:
+  else:
     print("NO VALID FORMAT!")
     sleep(1)
     exit
+
+url = input("Video URL : ")
+file_type = input("Type mp3 for audio and mp4 for video : ")
+download()
